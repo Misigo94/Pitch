e3da9da917c8b851bd65fe028b6d70a9f09e7c74
Misigomartin <Misigomartin@gmail.com>
2022-05-12 20:19:06 +0300

    change file structure

--- config.py
+++ config.py
@@ -1,15 +1,27 @@
-from distutils.command.config import config
 import os
 
 class Config:
-     SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://martin:Misigouts94@localhost:5432/Pitch'
-     DEBUG = True
+    DEBUG=True
+    SECRET_KEY="Password"
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+   
+
+
+ # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class DevConfig(Config):
-    pass
-
+     SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://user1:Password@localhost:5432/user1db'
+    
+    
+        
 class ProdConfig(Config):
+    #  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     pass
 
 
-
+configurations = {"production":ProdConfig, "development":DevConfig}
